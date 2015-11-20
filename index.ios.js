4ef79cc1f9d9843e4d51389e362df8fa171cc6f7
Mateusz Zatorski <mateuszzatorski@gmail.com>
2015-11-20 16:20:56 +0000

    :boom: first version of my RN app

--- index.ios.js
+++ index.ios.js
@@ -1,49 +1,128 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
+'use strict'
 
 import React, {
   AppRegistry,
   Component,
+  Image,
   StyleSheet,
   Text,
-  TextInput,
   View
 } from 'react-native'
+
+const TIME_TO_BUS_STOP = 180
+const MAX_WAIT_TIME = 120
+
+const APP_ID = 'YOUR_APP_ID'
+const APP_KEY = 'YOUR_APP_KEY'
+const STOP_ID = '490011297S'
+const API_URL = 'https://api.tfl.gov.uk/StopPoint/'
+const PARAMS = `?app_id=${APP_ID}&app_key=${APP_KEY}`
+const REQUEST_URL = `${API_URL}${STOP_ID}/Arrivals${PARAMS}`
+
+const PULLDOWN_DISTANCE = 40
 
 class BusReady extends Component {
   constructor() {
     super()
     this.state = {
-      text: ''
+      ready: false,
+      loaded: false,
+      nextBuses: null,
+      reloading: false,
+      readyBuses: null
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.checkBusReady(responseData)
+      })
+      .done()
+  }
+
+  checkBusReady(responseData) {
+    let busData = responseData.filter(bus =>
+      bus.lineId === '297' || bus.lineId === '302'
+    )
+
+    let readyBuses = busData.filter(bus =>
+      bus.timeToStation > TIME_TO_BUS_STOP && bus.timeToStation < (TIME_TO_BUS_STOP + MAX_WAIT_TIME)
+    )
+
+    let nextBuses = busData.filter(bus =>
+      bus.timeToStation > TIME_TO_BUS_STOP
+    )
+
+    nextBuses = nextBuses.sort((a, b) =>
+      a.timeToStation - b.timeToStation
+    )
+
+    if (readyBuses.length > 0) {
+      this.setState({
+        ready: true,
+        loaded: true,
+        readyBuses: readyBuses
+      })
+    } else {
+      this.setState({
+        ready: false,
+        loaded: true,
+        nextBuses: nextBuses
+      })
     }
   }
 
   render() {
+    const { ready, loaded, nextBuses, readyBuses } = this.state
+    let busInfo = null
+
+    if (!loaded) {
+      return (
+        <View style={styles.container}>
+          <Text>
+            Loading...
+          </Text>
+        </View>
+      )
+    }
+
+    if (nextBuses) {
+      busInfo = nextBuses.slice(0, 4).map((bus, i) =>
+        <View key={i} style={styles.busInfo}>
+          <Text style={styles.busInfoLine}>{ bus.lineId }</Text>
+          <Text style={styles.busInfoTime}>
+            in {Math.round(bus.timeToStation / 60)} mins
+          </Text>
+        </View>
+      )
+    }
+
+    if (readyBuses) {
+      busInfo = readyBuses.map((bus, i) =>
+        <View key={i} style={styles.busInfo}>
+          <Text style={styles.busInfoLine}>{ bus.lineId }</Text>
+          <Text style={styles.busInfoTime}>
+            in {Math.round(bus.timeToStation / 60)} mins
+          </Text>
+        </View>
+      )
+    }
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Yo!
+      <View style={ready ? styles.ready : styles.notReady}>
+        <Text
+          style={styles.busInfoTitle}
+          onPress={() => this.fetchData()}
+        >
+          Next buses:
         </Text>
-        <TextInput
-          style={{
-            height: 40,
-            width: 200,
-            borderColor: 'gray',
-            borderWidth: 1
-          }}
-          onChangeText={(text) => this.setState({text})}
-          value={this.state.text}
-        />
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        { busInfo }
       </View>
     )
   }
@@ -52,20 +131,43 @@ class BusReady extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: 'rgba(5, 165, 209, 0.05)',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  ready: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#27ae60'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  notReady: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e74c3c'
   },
-});
+  busInfoTitle: {
+    fontSize: 46,
+    color: '#fff'
+  },
+  busInfoLine: {
+    fontSize: 32,
+    color: '#fff'
+  },
+  busInfoTime: {
+    fontSize: 26,
+    left: 20,
+    color: '#fff'
+  },
+  busInfo: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
-AppRegistry.registerComponent('BusReady', () => BusReady);
+AppRegistry.registerComponent('BusReady', () => BusReady)
